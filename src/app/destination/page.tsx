41705fff55b8fd86bce716a3d02c21c9d8e7c480
JeremyAMichel <jeremy.a.michel@hotmail.fr>
2024-06-24 15:43:02 +0200

    DONE: Navbar

---
+++ src/app/destination/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>Destination</div>
+  )
+}
+
+export default page
---
+++ src/app/technology/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>Technology</div>
+  )
+}
+
+export default page
